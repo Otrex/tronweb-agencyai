7e44b1de4b2a366ba0e368321a5762c495fcaaac
Treasure <obisiket@yahoo.com>
2023-08-20 16:22:47 +0100

    feat: update

--- pages/jv.vue
+++ pages/jv.vue
@@ -19,6 +19,39 @@
 definePageMeta({
   layout: false,
 });
+
+useHead({
+  title: "AgencyAI JV",
+  link: [
+    {
+      rel: "image_src",
+      href: window.location.origin + '/meta/WebsitePreview.png'
+    },
+    { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "preconnect", href: "https://rsms.me/" },
+        { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
+  ],
+  meta: [
+    {
+      name: 'og:description',
+      hid: 'og:description',
+      content: "Launching Aug. 31st"
+    },
+    {
+      name: 'og:title',
+      hid: 'og:title',
+      content: "AgencyAI JV"
+    },
+    {
+      name: 'og:image',
+      hid: 'og:image',
+      content: window.location.origin + '/meta/WebsitePreview.png'
+    }
+  ]
+})
 </script>
 
 <style></style>
